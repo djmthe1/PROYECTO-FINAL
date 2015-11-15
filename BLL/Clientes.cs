28e549829f417c65c2533a97703821f0ac7d9892
djmthe1 <mcgraththe1@gmail.com>
2015-11-14 23:34:41 -0400

    subiendo clase cliente y vehiculo que se quedaron

---
+++ BLL/Clientes.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Clientes : ClaseMaestra
+    {
+
+        public int ClienteId { set; get; }
+        public string NombreCompleto { set; get; }
+        public string Apodo { set; get; }
+        public int TelefonoId { set; get; }
+        public string Direccion { set; get; }
+        public string Cedula { set; get; }
+        public string Nacionalidad { set; get; }
+        public string Ocupacion { set; get; }
+        public string LugarDeNacimiento { set; get; }
+        public string Sexo { set; get; }
+        public int FacturaId { set; get; }
+        public ConexionDb conexion = new ConexionDb();
+
+        public Clientes(int clienteId, string nombreCompleto, string Apodo, int telefonoId, string direccion, string cedula, string nacionalidad, string ocupacion, string lugarDeNacimiento, string sexo, int facturaId) {
+            this.ClienteId = clienteId;
+            this.NombreCompleto = nombreCompleto;
+            this.Apodo = Apodo;
+            this.TelefonoId = telefonoId;
+            this.Direccion = direccion;
+            this.Cedula = cedula;
+            this.Nacionalidad = nacionalidad;
+            this.Ocupacion = ocupacion;
+            this.LugarDeNacimiento = lugarDeNacimiento;
+            this.Sexo = sexo;
+            this.FacturaId = facturaId;
+        }
+
+        public Clientes() { }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            DataTable dt = new DataTable();
+
+            dt = conexion.ObtenerDatos("Select * from Clientes Where ClienteId=" + IdBuscado);
+            if (dt.Rows.Count > 0)
+            {
+                this.ClienteId = (int)dt.Rows[0]["ClienteId"];
+                this.NombreCompleto = dt.Rows[0]["NombreCompleto"].ToString();
+                this.Apodo = dt.Rows[0]["Apodo"].ToString();
+                this.TelefonoId = (int)dt.Rows[0]["TelefonoId"];
+                this.Direccion = dt.Rows[0]["Direccion"].ToString();
+                this.Cedula = dt.Rows[0]["Cedula"].ToString();
+                this.Nacionalidad = dt.Rows[0]["Nacionalidad"].ToString();
+                this.Ocupacion = dt.Rows[0]["Ocupacion"].ToString();
+                this.LugarDeNacimiento = dt.Rows[0]["LugarDeNacimiento"].ToString();
+                this.Sexo = dt.Rows[0]["Sexo"].ToString();
+                this.FacturaId = (int)dt.Rows[0]["FacturaId"];
+            }
+            return dt.Rows.Count > 0;
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Update Clientes set (NombreCompleto,Apodo,TelefonoId,Direccion,Cedula,Nacionalidad,Ocupacion,LugarDeNacimiento,Sexo,FacturaId) Values('{0}'{1}'{2}'{3}'{4}'{5}'{6}'{7}'{8}'{9}') where ClienteId={10}", this.NombreCompleto, this.Apodo, this.TelefonoId, this.Direccion, this.Cedula, this.Nacionalidad, this.Ocupacion, this.LugarDeNacimiento, this.Sexo, this.FacturaId, this.ClienteId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Delete From Clientes where ClienteId={0}", this.ClienteId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("insert into Clientes (NombreCompleto,Apodo,TelefonoId,Direccion,Cedula,Nacionalidad,Ocupacion,LugarDeNacimiento,Sexo,FacturaId) Values('{0}'{1}'{2}'{3}'{4}'{5}'{6}'{7}'{8}'{9}')", this.NombreCompleto, this.Apodo, this.TelefonoId, this.Direccion, this.Cedula, this.Nacionalidad, this.Ocupacion, this.LugarDeNacimiento, this.Sexo, this.FacturaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenar = "";
+            if (!Orden.Equals(""))
+                ordenar = " orden by  " + Orden;
+            return conexion.ObtenerDatos(("Select " + Campos + " from Clientes where " + Condicion + ordenar));
+        }
+    }
+}
---
+++ BLL/Vehiculos.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Vehiculos : ClaseMaestra
+    {
+
+        public int VehiculoId { set; get; }
+        public int EstadoDelVehiculoId { set; get; }
+        public int AtributosId { set; get; }
+        public int ModelosId { set; get; }
+        public int MarcasId { set; get; }
+        public int MotorId { set; get; }
+        public int ColorId { set; get; }
+        public int Año { set; get; }
+        public string NoChasis { set; get; }
+        public int TipoDeVehiculoId { set; get; }
+        public int Kilometraje { set; get; }
+        public int Precio { set; get; }
+        public string Placa { set; get; }
+        public string Matricula { set; get; }
+        public ConexionDb conexion = new ConexionDb();
+
+        public Vehiculos(int vehiculoId, int estadoDelVehiculoId, int atributosId, int modeloId, int marcaId, int motorId, int colorId, int año, string noChasis, int tipoDeVehiculoId, int kilometraje, int precio, string placa, string matricula) {
+            this.VehiculoId = vehiculoId;
+            this.EstadoDelVehiculoId = estadoDelVehiculoId;
+            this.AtributosId = atributosId;
+            this.ModelosId = modeloId;
+            this.MarcasId = marcaId;
+            this.MotorId = motorId;
+            this.ColorId = colorId;
+            this.Año = año;
+            this.NoChasis = noChasis;
+            this.TipoDeVehiculoId = tipoDeVehiculoId;
+            this.Kilometraje = kilometraje;
+            this.Precio = precio;
+            this.Placa = placa;
+            this.Matricula = matricula;
+        }
+
+        public Vehiculos() { }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            DataTable dt = new DataTable();
+
+            dt = conexion.ObtenerDatos("Select * from Vehiculos Where VehiculoId=" + IdBuscado);
+            if (dt.Rows.Count > 0)
+            {
+                this.VehiculoId = (int)dt.Rows[0]["VehiculoId"];
+                this.EstadoDelVehiculoId = (int)dt.Rows[0]["EstadoDelVehiculoId"];
+                this.AtributosId = (int)dt.Rows[0]["AtributosId"];
+                this.ModelosId = (int)dt.Rows[0]["ModelosId"];
+                this.MarcasId = (int)dt.Rows[0]["MarcasId"];
+                this.MotorId = (int)dt.Rows[0]["MotorId"];
+                this.ColorId = (int)dt.Rows[0]["ColorId"];
+                this.Año = (int)dt.Rows[0]["Año"];
+                this.NoChasis = dt.Rows[0]["NoChasis"].ToString();
+                this.TipoDeVehiculoId = (int)dt.Rows[0]["TipoDeVehiculoId"];
+                this.Kilometraje = (int)dt.Rows[0]["Kilometraje"];
+                this.Precio = (int)dt.Rows[0]["Precio"];
+                this.Placa = dt.Rows[0]["Placa"].ToString();
+                this.Matricula = dt.Rows[0]["Matricula"].ToString();
+            }
+            return dt.Rows.Count > 0;
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Update Vehiculos set (EstadoDelVehiculoId, AtributosId, ModelosId, MarcasId, MotorId, ColorId, Año, NoChasis, TipoDeVehiculoId, Kilometraje, Precio, Placa, Matricula) Values('{0}'{1}'{2}'{3}'{4}'{5}'{6}'{7}'{8}'{9}'{10}'{11}'{12}') where VehiculoId={13}", this.EstadoDelVehiculoId, this.AtributosId, this.ModelosId, this.MarcasId, this.MotorId, this.ColorId, this.Año, this.NoChasis, this.TipoDeVehiculoId, this.Kilometraje, this.Precio, this.Placa, this.Matricula, this.VehiculoId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Delete From Vehiculos where VehiculoId={0}", this.VehiculoId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Insert Into Vehiculos (EstadoDelVehiculoId, AtributosId, ModelosId, MarcasId, MotorId, ColorId, Año, NoChasis, TipoDeVehiculoId, Kilometraje, Precio, Placa, Matricula) Values('{0}'{1}'{2}'{3}'{4}'{5}'{6}'{7}'{8}'{9}'{10}'{11}'{12}')", this.EstadoDelVehiculoId, this.AtributosId, this.ModelosId, this.MarcasId, this.MotorId, this.ColorId, this.Año, this.NoChasis, this.TipoDeVehiculoId, this.Kilometraje, this.Precio, this.Placa, this.Matricula));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenar = "";
+            if (!Orden.Equals(""))
+                ordenar = " orden by  " + Orden;
+            return conexion.ObtenerDatos(("Select " + Campos + " from Vehiculos where " + Condicion + ordenar));
+        }
+    }
+}
