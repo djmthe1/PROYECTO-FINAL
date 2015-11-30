98748d0969289d78b38963b8ea9749cdb8387ebd
hamlet3 <hamletreyes3@gmail.com>
2015-11-29 18:39:54 -0600

    arreglando conflicto

--- Sistema-Ventas-Vehiculos/Registros/ListarForm.Designer.cs
+++ Sistema-Ventas-Vehiculos/Registros/ListarForm.Designer.cs
@@ -31,6 +31,7 @@
             this.listarDataGridView = new System.Windows.Forms.DataGridView();
             this.listarComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.listarDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,8 +55,9 @@
             "Vehiculos"});
             this.listarComboBox.Location = new System.Drawing.Point(12, 35);
             this.listarComboBox.Name = "listarComboBox";
-            this.listarComboBox.Size = new System.Drawing.Size(121, 21);
+            this.listarComboBox.Size = new System.Drawing.Size(94, 21);
             this.listarComboBox.TabIndex = 37;
+            this.listarComboBox.Text = "Buscar por . . .";
             this.listarComboBox.SelectedIndexChanged += new System.EventHandler(this.listarComboBox_SelectedIndexChanged);
             // 
             // label1
@@ -68,12 +70,30 @@
             this.label1.TabIndex = 38;
             this.label1.Text = "LISTADO";
             // 
+            // FiltroComboBox
+            // 
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Items.AddRange(new object[] {
+            "Marcas",
+            "Modelos",
+            "Colores",
+            "Tipos de Vehiculos",
+            "Tipos de Motores",
+            "Vehiculos"});
+            this.FiltroComboBox.Location = new System.Drawing.Point(112, 35);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(94, 21);
+            this.FiltroComboBox.TabIndex = 39;
+            this.FiltroComboBox.Text = "Filtrar por . . .";
+            this.FiltroComboBox.SelectedIndexChanged += new System.EventHandler(this.FiltroComboBox_SelectedIndexChanged);
+            // 
             // ListarForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(404, 275);
+            this.Controls.Add(this.FiltroComboBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listarComboBox);
             this.Controls.Add(this.listarDataGridView);
@@ -93,5 +113,6 @@
         private System.Windows.Forms.DataGridView listarDataGridView;
         public System.Windows.Forms.ComboBox listarComboBox;
         private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ComboBox FiltroComboBox;
     }
 }
